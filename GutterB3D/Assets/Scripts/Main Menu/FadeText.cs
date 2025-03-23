e756e2a8b383ff13ed743129bdcb50a03faf6f6d
Euluer01 <euluer01@tufts.edu>
2025-03-23 15:44:57 -0400

    Main Menu

---
+++ GutterB3D/Assets/Scripts/Main Menu/FadeText.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FadeTextOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Graphic uiText; // Can be Text, TextMeshProUGUI, etc.
+    public float fadeDuration = 0.5f;
+    private Color originalColor;
+    private Coroutine fadeCoroutine;
+
+    void Start()
+    {
+        if (uiText != null)
+            originalColor = uiText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeToAlpha(0.3f));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeToAlpha(1f));
+    }
+
+    private System.Collections.IEnumerator FadeToAlpha(float targetAlpha)
+    {
+        float startAlpha = uiText.color.a;
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, time / fadeDuration);
+            uiText.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        uiText.color = new Color(originalColor.r, originalColor.g, originalColor.b, targetAlpha);
+    }
+}
