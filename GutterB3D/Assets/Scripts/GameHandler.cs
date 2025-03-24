1cebf8ca5e5aa9f91c4553df9fcdf33bafc9362d
maddalenaseganti <mseganti062205@gmail.com>
2025-03-23 20:33:13 -0400

    Added Options Menu, Gamehandler
    Volume control doesn't work yet

---
+++ GutterB3D/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    public void StartGame() {
+            SceneManager.LoadScene("Level1");
+      }
+
+      // Return to MainMenu
+      public void MainMenu() {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+      }
+
+      public void QuitGame() {
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+      }
+
+      public void Credits() {
+            SceneManager.LoadScene("Credits");
+      }
+}
