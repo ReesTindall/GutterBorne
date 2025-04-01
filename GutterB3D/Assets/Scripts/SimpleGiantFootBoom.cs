520f083d7f00b95c9b7b28159b44d23eb5be6a34
v1v14nb <156724407+v1v14nb@users.noreply.github.com>
2025-04-01 13:18:17 -0400

    updates

--- GutterB3D/Assets/Scripts/SimpleGiantFootBoom.cs
+++ GutterB3D/Assets/Scripts/SimpleGiantFootBoom.cs
@@ -10,6 +10,9 @@ public class SimpleGiantFootBoom : MonoBehaviour
     private float clipVolume;            // hold and modify the volume
 
     public GameObject groundObject;      // Drag your ground object here in the Inspector
+    public float boomInterval = 2f;      // Time between automatic booms
+
+    private float boomTimer;
 
     void Awake()
     {
@@ -18,12 +21,33 @@ public class SimpleGiantFootBoom : MonoBehaviour
         clipStartPitch = audioClip.pitch;
         clipPitch = audioClip.pitch;
         clipVolume = audioClip.volume;
+
+        boomTimer = boomInterval;
+    }
+
+    void Update()
+    {
+        boomTimer -= Time.deltaTime;
+
+        if (boomTimer <= 0f)
+        {
+            PlayBoom();
+            boomTimer = boomInterval;
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
         // Only play sound if colliding with the specified ground object
         if (other.gameObject == groundObject)
+        {
+            PlayBoom();
+        }
+    }
+
+    void PlayBoom()
+    {
+        if (!audioClip.isPlaying)
         {
             audioClip.Play();
         }
