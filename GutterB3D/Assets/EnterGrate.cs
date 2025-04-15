33d46ef07f4e9b764f99e4ab0be37f95107aae9e
maddalenaseganti <mseganti062205@gmail.com>
2025-04-14 22:49:58 -0400

    working grate entrances
    add tag "Entrance" to grates w flag/ lead to next level

---
+++ GutterB3D/Assets/EnterGrate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterGrate : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Entrance")) {
+            int nextSceneIdx = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(nextSceneIdx);
+        }
+    }
+}
