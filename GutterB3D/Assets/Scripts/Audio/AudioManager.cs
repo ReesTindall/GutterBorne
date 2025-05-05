a9bfb69aa803fe194772571234ceef5061c2ab75
Euluer01 <euluer01@tufts.edu>
2025-05-05 10:53:06 -0400

    sliderzs attached

--- GutterB3D/Assets/Scripts/Audio/AudioManager.cs
+++ GutterB3D/Assets/Scripts/Audio/AudioManager.cs
@@ -2,38 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-//INCLUDE IN EVERY SCENE
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
-    [SerializeField] AudioMixer mixer;
+    [Header("Mixer + Sliders")]
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
 
-    public const string MUSIC_KEY = "VolumeMusic";
-    public const string SFX_KEY = "VolumeSFX";
+    private const string MUSIC_KEY = "VolumeMusic";
+    private const string SFX_KEY = "VolumeSFX";
+    private const string MUSIC_PARAM = "MusicVolume";
+    private const string SFX_PARAM = "SFXVolume";
 
-    void Awake() {
-        //if player set volume, load across scenes
-
-        if(instance == null) {
+    void Awake()
+    {
+        if (instance == null)
+        {
             instance = this;
             DontDestroyOnLoad(gameObject);
-
-        } else {
+        }
+        else
+        {
             Destroy(gameObject);
+            return;
         }
 
         LoadVolume();
     }
 
-    //the volumes are saved in VolumeSettings.cs
-    void LoadVolume() {
+    void Start()
+    {
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
 
-        float volumeMusic = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
-        float volumeSFX = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+    void LoadVolume()
+    {
+        float musicVol = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfxVol = PlayerPrefs.GetFloat(SFX_KEY, 1f);
 
-        mixer.SetFloat(VolumeSettings.MUSIC, Mathf.Log10(volumeMusic) * 20);
-        mixer.SetFloat(VolumeSettings.SFX, Mathf.Log10(volumeSFX) * 20);
+        musicSlider.value = musicVol;
+        sfxSlider.value = sfxVol;
+
+        mixer.SetFloat(MUSIC_PARAM, Mathf.Log10(Mathf.Clamp(musicVol, 0.0001f, 1f)) * 20);
+        mixer.SetFloat(SFX_PARAM, Mathf.Log10(Mathf.Clamp(sfxVol, 0.0001f, 1f)) * 20);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        mixer.SetFloat(MUSIC_PARAM, Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20);
+        PlayerPrefs.SetFloat(MUSIC_KEY, value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        mixer.SetFloat(SFX_PARAM, Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20);
+        PlayerPrefs.SetFloat(SFX_KEY, value);
     }
 }
