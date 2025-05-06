a049c8ef37b8772ffc3fe184f68825cf44f5ea07
Euluer01 <euluer01@tufts.edu>
2025-05-06 02:17:34 -0400

    retry works now

--- GutterB3D/Assets/Scripts/Main Menu/GameHandler.cs
+++ GutterB3D/Assets/Scripts/Main Menu/GameHandler.cs
@@ -14,28 +14,29 @@ public class GameHandler : MonoBehaviour
       private static float prevLevelSlimeSize = 1f;
       private SlimeSizeBar bar;
 
-	//for cutscenes:
-	public static int levelNumber = 1;
+    //for cutscenes:
+    public static int levelNumber = 1;
 
-      //checks to make sure there is only one game handler in scene
-	  /*
-      void Awake() {
-            if(handler == null) {
-                  handler = this;
-                  DontDestroyOnLoad(gameObject);
-            } else {
-                  Destroy(gameObject);
-            }
-      }
-	  */
+    //checks to make sure there is only one game handler in scene
+    /*
+    void Awake() {
+          if(handler == null) {
+                handler = this;
+                DontDestroyOnLoad(gameObject);
+          } else {
+                Destroy(gameObject);
+          }
+    }
+    */
 
-	void Start(){
+    void Start(){
 		//Cutscene stuff to send player to next level:
 		Scene scene = SceneManager.GetActiveScene();
 		if (scene.name == "Sewer_Cutscene1"){
 			levelNumber +=1;
                   prevLevelSlimeSize = slimeSize; //save slime size before new level starts
-			GameObject.FindWithTag("Canvas").GetComponent<Cutscene_Controller>().nextLevelName = "Level" + levelNumber.ToString();
+            Debug.Log("Level before switch: " + levelNumber);
+            GameObject.FindWithTag("Canvas").GetComponent<Cutscene_Controller>().nextLevelName = "Level" + levelNumber.ToString();
 		}
 	}
 
@@ -99,6 +100,8 @@ public class GameHandler : MonoBehaviour
         }
 
         Debug.Log("After slime size change:" + slimeSize);
+
+        Debug.Log("Level before switch: " + levelNumber);
         SceneManager.LoadScene("Level" + levelNumber.ToString());
     }
 }
